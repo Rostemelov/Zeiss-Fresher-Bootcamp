0295d7e59d8c9d89dc1535438c1eae1690c4f30c
Sai Harshit.B <89305971+Rostemelov@users.noreply.github.com>
2024-01-10 15:14:01 +0530

    Update fitler_oop.py

--- day3/fitler_oop.py
+++ day3/fitler_oop.py
@@ -1,59 +1,59 @@
 #-------------------------------------------------------------------------------    
 class StartPredicates:
     def __init__(self, startlist):
-        self.startlist = startlist
+        self.__startlist = startlist
     def check_string_starting_with(self, string):
-        if string[0] in self.startlist:
+        if string[0] in self.__startlist:
             return True
         else:
             return False
 #-------------------------------------------------------------------------------
 class EndPredicates:
     def __init__(self, endlist):
-        self.endlist = endlist
+        self.__endlist = endlist
     def check_string_ending_with(self, string):
-        if string[-1] in self.endlist:
+        if string[-1] in self.__endlist:
             return True
         else:
             return False
 #-------------------------------------------------------------------------------
 class Filter:
     def __init__(self, startlist, endlist):
-        self.answer_list = []
-        self.stprd = StartPredicates(startlist)
-        self.stpprd = EndPredicates(endlist)
+        self.__answer_list = []
+        self.__stprd = StartPredicates(startlist)
+        self.__stpprd = EndPredicates(endlist)
     def filter(self, input_str, criteria_function):
         for string in input_str:
             if criteria_function == "start":
-                if(self.stprd.check_string_starting_with(string)):
-                    self.answer_list.append(string)
+                if(self.__stprd.check_string_starting_with(string)):
+                    self.__answer_list.append(string)
             elif criteria_function == "end":
-                if(self.stpprd.check_string_ending_with(string)):
-                    self.answer_list.append(string)
-        return self.answer_list
+                if(self.__stpprd.check_string_ending_with(string)):
+                    self.__answer_list.append(string)
+        return self.__answer_list
 #-------------------------------------------------------------------------------
 class Printer:
     def __init__(self):
-        self.content = []
+        self.__content = []
     def setContent(self, l):
-        self.content = l
+        self.__content = l
     def printToTerminal(self):
-        for string in self.content:
+        for string in self.__content:
             print(string)
 #-------------------------------------------------------------------------------
 class Initiator:
     def __init__(self):
-        self.array_of_strings = ['Abhishek','abhinav','Deepanshu','Vishal','Manisha','Dadlani']
-        self.printer = Printer()
-        self.filt = Filter(['a','A','d','V','p'], ['a','A','d','V','p'])
+        self.__array_of_strings = ['Abhishek','abhinav','Deepanshu','Vishal','Manisha','Dadlani']
+        self.__printer = Printer()
+        self.__filt = Filter(['a','A','d','V','p'], ['a','A','d','V','p'])
         
     def dowork(self):
-        result = self.filt.filter(self.array_of_strings, "start")
-        self.printer.setContent(result)
-        self.printer.printToTerminal()
-        result = self.filt.filter(self.array_of_strings, "end")
-        self.printer.setContent(result)
-        self.printer.printToTerminal()
+        result = self.__filt.filter(self.__array_of_strings, "start")
+        self.__printer.setContent(result)
+        self.__printer.printToTerminal()
+        result = self.__filt.filter(self.__array_of_strings, "end")
+        self.__printer.setContent(result)
+        self.__printer.printToTerminal()
          
 #-------------------------------------------------------------------------------
 init1 = Initiator()     
