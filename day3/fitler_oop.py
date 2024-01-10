883803d3c81008201eeab3a817bb52de3d140465
Sai Harshit.B <89305971+Rostemelov@users.noreply.github.com>
2024-01-10 11:14:34 +0530

    Update fitler_oop.py
    removed bugs

--- day3/fitler_oop.py
+++ day3/fitler_oop.py
@@ -1,23 +1,27 @@
 #-------------------------------------------------------------------------------    
 class Filter:
-    def filter(self, input_str, criteria_function):
+    def filter(self, input_str, criteria_function, char):
         answer_list = []
         for string in input_str:
             if criteria_function == "start":
-                s = self.check_string_starting_with(string)
-                answer_list.append(s)
+                if(self.check_string_starting_with(string, char)):
+                    answer_list.append(string)
             elif criteria_function == "end":
-                s = self.check_string_ending_with(string)
-                answer_list.append(s)
+                if(self.check_string_ending_with(string, char)):
+                    answer_list.append(string)
         return answer_list
         
-    def check_string_starting_with(self, char):
-        predicate = lambda string : string[0] == char
-        return predicate
+    def check_string_starting_with(self, string, char):
+        if string[0] == char:
+            return True
+        else:
+            return False
     
-    def check_string_ending_with(self, char):
-        predicate = lambda string : string[-1] == char
-        return predicate
+    def check_string_ending_with(self, string, char):
+        if string[-1] == char:
+            return True
+        else:
+            return False
 #-------------------------------------------------------------------------------
 class Printer:
     def printToTerminal(self, output_str_list):
@@ -33,10 +37,10 @@ class Initiator:
         
     def dowork(self):
         for char in self.charay:
-            result = self.filt.filter(self.array_of_strings, "start")
+            result = self.filt.filter(self.array_of_strings, "start", char)
             self.printer.printToTerminal(result)
         for char in self.charay:
-            result = self.filt.filter(self.array_of_strings, "end")
+            result = self.filt.filter(self.array_of_strings, "end", char)
             self.printer.printToTerminal(result)
          
 init1 = Initiator()     
